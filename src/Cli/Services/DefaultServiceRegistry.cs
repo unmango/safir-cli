ac8f41f4f558e9ac97673c05034b0443787c5e32
Erik Rasmussen <ecr.referee@hotmail.com>
2020-12-28 16:36:10 -0600

    Kinda finish default service registry impl

--- src/Cli/Services/DefaultServiceRegistry.cs
+++ src/Cli/Services/DefaultServiceRegistry.cs
@@ -1,22 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cli.Services
 {
     internal class DefaultServiceRegistry : IServiceRegistry
     {
+        private readonly IServiceFactory _factory;
         private readonly List<IServiceSource> _sources;
 
-        public DefaultServiceRegistry(IEnumerable<IServiceSource> sources)
+        public DefaultServiceRegistry(IServiceFactory factory, IEnumerable<IServiceSource> sources)
         {
+            _factory = factory;
             _sources = sources.ToList();
         }
 
         public IReadOnlyList<ServiceEntry> Services { get; } = new List<ServiceEntry>();
 
-        public Task<IService> GetServiceAsync(ServiceEntry service)
+        public async Task<IService> GetServiceAsync(ServiceEntry service, CancellationToken cancellationToken = default)
         {
             if (service == null) throw new ArgumentNullException(nameof(service));
 
@@ -25,7 +28,18 @@ namespace Cli.Services
             if (source == null)
                 throw new NotSupportedException($"No service source found to support service {service.Name}");
 
-            throw new System.NotImplementedException();
+            var canInitialize = await source.CanInitializeAsync(cancellationToken);
+
+            if (canInitialize.Failed)
+                throw new NotSupportedException(
+                    $"Can't initialize source {source.GetType()}: {canInitialize.FailureMessage}");
+
+            var workingDirectory = string.Empty; // TODO
+
+            if (!await source.IsInitializedAsync(service, workingDirectory, cancellationToken))
+                await source.InitializeAsync(service, workingDirectory, cancellationToken);
+
+            return _factory.Create(service);
         }
     }
 }
