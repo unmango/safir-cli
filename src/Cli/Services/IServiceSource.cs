37b15fd0654778e440ee35c7f0fbb233b5122bba
Erik Rasmussen <ecr.referee@hotmail.com>
2020-12-28 15:56:32 -0600

    Fleshing out service source

--- src/Cli/Services/IServiceSource.cs
+++ src/Cli/Services/IServiceSource.cs
@@ -1,7 +1,14 @@
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Cli.Services
 {
     internal interface IServiceSource
     {
+        ValueTask<CanInitializeResult> CanInitializeAsync(CancellationToken cancellationToken = default);
+
+        Task InitializeAsync(string workingDirectory, CancellationToken cancellationToken = default);
         
+        bool Satisfies(ServiceEntry service);
     }
 }
