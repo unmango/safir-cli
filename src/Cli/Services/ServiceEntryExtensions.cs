88e65238d94c9b4a9198fc55c3b328637eedc768
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-04 21:28:58 -0600

    Just remove all service entry extensions for now

--- src/Cli/Services/ServiceEntryExtensions.cs
+++ src/Cli/Services/ServiceEntryExtensions.cs
@@ -6,27 +6,5 @@ namespace Cli.Services
 {
     internal static class ServiceEntryExtensions
     {
-        public static IServiceInstaller GetInstaller(this ServiceEntry service)
-        {
-            // return service.Sources.OrderByPriority().Single().GetInstaller();
-            throw new NotImplementedException();
-        }
-
-        public static IServiceInstaller? GetGitInstaller(this ServiceEntry service)
-        {
-            return service.Sources.HighestPriorityOrDefault(x => x.Type == SourceType.Git)?.GetGitInstaller();
-        }
-
-        public static IServiceInstaller? GetDotnetToolInstaller(this ServiceEntry service)
-        {
-            return service.Sources.HighestPriorityOrDefault(x => x.Type == SourceType.DotnetTool)
-                ?.GetDotnetToolInstaller();
-        }
-
-        public static IServiceInstaller? GetLocalDirectoryInstaller(this ServiceEntry service)
-        {
-            return service.Sources.HighestPriorityOrDefault(x => x.Type == SourceType.LocalDirectory)
-                ?.GetLocalDirectoryInstaller();
-        }
     }
 }
