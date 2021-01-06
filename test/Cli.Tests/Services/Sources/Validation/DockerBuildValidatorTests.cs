0f67ac40e5797908a964be5c7516c71bf184c935
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-05 22:25:49 -0600

    Add docker build validator tests

---
+++ test/Cli.Tests/Services/Sources/Validation/DockerBuildValidatorTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using Cli.Services;
+using Cli.Services.Installers;
+using Cli.Services.Sources.Validation;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Cli.Tests.Services.Sources.Validation
+{
+    public class DockerBuildValidatorTests
+    {
+        private readonly DockerBuildValidator _validator = new();
+        
+        [Theory]
+        [MemberData(nameof(SourceTypeValuesExcept), SourceType.DockerBuild)]
+        public void RequiresDockerBuildSourceType(SourceType type)
+        {
+            var source = new ServiceSource { Type = type };
+
+            var result = _validator.TestValidate(source);
+
+            result.ShouldHaveValidationErrorFor(x => x.Type);
+        }
+
+        [Theory]
+        [ClassData(typeof(NullOrWhitespaceStrings))]
+        public void RequiresBuildContext(string? buildContext)
+        {
+            var source = new ServiceSource {
+                Type = SourceType.DockerBuild,
+                BuildContext = buildContext,
+            };
+
+            var result = _validator.TestValidate(source);
+
+            result.ShouldHaveValidationErrorFor(x => x.BuildContext);
+        }
+
+        [Fact]
+        public void GetsDockerBuildInstaller()
+        {
+            var source = new ServiceSource {
+                Type = SourceType.DockerBuild,
+                BuildContext = "context",
+            };
+
+            var result = _validator.TestValidate(source);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        private static IEnumerable<object[]> SourceTypeValuesExcept(SourceType type) => SourceTypeValues.Except(type);
+    }
+}
