7069f606a423161225a53821f557fceb71bd36e5
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-07 00:16:12 -0600

    Test out interface with git installer

--- src/Cli/Services/Installers/GitInstaller.cs
+++ src/Cli/Services/Installers/GitInstaller.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Cli.Services.Sources;
+
 // ReSharper disable NotAccessedField.Local
 
 namespace Cli.Services.Installers
 {
-    internal class GitInstaller : IServiceInstaller
+    internal class GitInstaller : IServiceInstaller, IPipelineServiceInstaller
     {
         private readonly string _cloneUrl;
 
@@ -12,10 +16,23 @@ namespace Cli.Services.Installers
         {
             _cloneUrl = cloneUrl;
         }
-        
+
         public ValueTask InstallAsync(InstallationContext context, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
+
+        public bool AppliesTo(InstallationContext context)
+        {
+            return context.Sources.Any(x => x.TryGetGitSource(out _));
+        }
+
+        public ValueTask InvokeAsync(
+            InstallationContext context,
+            Func<InstallationContext, ValueTask> next,
+            CancellationToken cancellationToken = default)
+            => AppliesTo(context)
+                ? InstallAsync(context, cancellationToken)
+                : next(context);
     }
 }
