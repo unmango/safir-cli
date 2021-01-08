56d3b3f830d8539052bc6363df9fe9dbf510c5f0
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-07 23:01:31 -0600

    Kinda working service

--- src/Cli/Internal/PipelineInstallationService.cs
+++ src/Cli/Internal/PipelineInstallationService.cs
@@ -1,19 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Cli.Services;
+using Microsoft.Extensions.Logging;
 
 namespace Cli.Internal
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     internal class PipelineInstallationService : IInstallationService
     {
-        private readonly IServiceDirectory _serviceDirectory;
+        private static readonly Func<InstallationContext, CancellationToken, ValueTask> _seed =
+            (_, _) => ValueTask.CompletedTask;
 
-        public PipelineInstallationService(IServiceDirectory serviceDirectory)
+        private readonly IServiceDirectory _serviceDirectory;
+        private readonly IEnumerable<IPipelineServiceInstaller> _installers;
+        private readonly ILogger<PipelineInstallationService> _logger;
+
+        public PipelineInstallationService(
+            IServiceDirectory serviceDirectory,
+            IEnumerable<IPipelineServiceInstaller> installers,
+            ILogger<PipelineInstallationService> logger)
         {
-            _serviceDirectory = serviceDirectory;
+            _serviceDirectory = serviceDirectory ?? throw new ArgumentNullException(nameof(serviceDirectory));
+            _installers = installers ?? throw new ArgumentNullException(nameof(installers));
+            _logger = logger;
         }
-        
-        public Task InstallAsync(
+
+        public async Task InstallAsync(
             ServiceEntry service,
             string? directory = null,
             CancellationToken cancellationToken = default)
@@ -26,8 +41,20 @@ namespace Cli.Internal
                 service,
                 // TODO: Select sources
                 service.Sources);
-            
-            throw new System.NotImplementedException();
+
+            var pipeline = CreatePipeline(_installers);
+            await pipeline(context, cancellationToken);
         }
+
+        private static Func<InstallationContext, CancellationToken, ValueTask> CreatePipeline(
+            IEnumerable<IPipelineServiceInstaller> installers)
+            => installers.Aggregate(
+                _seed,
+                (pipeline, installer)
+                    => (context, cancellationToken)
+                        => installer.InvokeAsync(
+                            context,
+                            innerContext => pipeline(innerContext, cancellationToken),
+                            cancellationToken));
     }
 }
