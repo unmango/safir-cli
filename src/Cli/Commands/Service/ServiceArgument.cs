bfb8c8d2971d44e97b2abdbf03364ada95c37c3b
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-08 22:38:54 -0600

    Allow for overriding description on ServiceArgument

--- src/Cli/Commands/Service/ServiceArgument.cs
+++ src/Cli/Commands/Service/ServiceArgument.cs
@@ -6,13 +6,19 @@ namespace Cli.Commands.Service
 {
     internal sealed class ServiceArgument : Argument<string>
     {
+        private const string DefaultDescription = "The name of the service the manage";
+        
         private static readonly string[] _services = {
             "manager",
             "listener",
         };
+
+        public ServiceArgument(string description) : this(null, description)
+        {
+        }
         
-        public ServiceArgument(IEnumerable<string>? services = null)
-            : base("service", "The name of the service the modify")
+        public ServiceArgument(IEnumerable<string>? services = null, string? description = null)
+            : base("service", description ?? DefaultDescription)
         {
             Arity = ArgumentArity.OneOrMore;
 
