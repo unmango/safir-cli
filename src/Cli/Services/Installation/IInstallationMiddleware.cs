c50ae6258d77fba1268fe4713dfa718041d259bd
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-09 16:35:25 -0600

    Create typed installation middleware delegates

--- src/Cli/Services/Installation/IInstallationMiddleware.cs
+++ src/Cli/Services/Installation/IInstallationMiddleware.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Cli.Internal.Pipeline;
 
 namespace Cli.Services.Installation
 {
+    internal delegate bool AppliesTo(InstallationContext context);
+    
+    internal delegate ValueTask InvokeAsync(
+        InstallationContext context,
+        Func<InstallationContext, ValueTask> next,
+        CancellationToken cancellationToken);
+    
     internal interface IInstallationMiddleware : IPipelineBehaviour<InstallationContext>
     {
     }
