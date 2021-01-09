d981bf6ec252a401c396ca6a1b4d2fc24cd093a3
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-08 19:35:45 -0600

    Add helper extension for building pipeline

--- src/Cli/Internal/PipelineBehaviourExtensions.cs
+++ src/Cli/Internal/PipelineBehaviourExtensions.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Cli.Internal
 {
     internal static class PipelineBehaviourExtensions
     {
+        public static InvokeAsync<T> BuildPipeline<T>(this IEnumerable<IPipelineBehaviour<T>> behaviours)
+            where T : class
+            => behaviours.Select(x => x.GetInvokeDelegate()).Aggregate(
+                (first, second)
+                    => (context, next, cancellationToken)
+                        => first(
+                            context,
+                            innerContext => second(innerContext, next, cancellationToken),
+                            cancellationToken));
+        
         public static AppliesTo<T> GetAppliesToDelegate<T>(this IPipelineBehaviour<T> behaviour)
             where T : class
             => behaviour.AppliesTo;
