52b6b00549b93b0f749cf34b8f5683619c973ef4
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-09 00:35:47 -0600

    Add method for logging unmatched services

--- src/Cli/Commands/Service/InstallLoggerExtensions.cs
+++ src/Cli/Commands/Service/InstallLoggerExtensions.cs
@@ -12,22 +12,33 @@ namespace Cli.Commands.Service
                 new EventId(1, nameof(ServicesToInstall)),
                 "Selected services to install: {Services}");
 
+        private static readonly Action<ILogger, IEnumerable<string>, Exception?> _unmatchedServices =
+            LoggerMessage.Define<IEnumerable<string>>(
+                LogLevel.Debug,
+                new EventId(2, nameof(UnmatchedServices)),
+                "Services not matched: {Services}");
+
         private static readonly Action<ILogger, Exception?> _installationSucceeded =
             LoggerMessage.Define(
                 LogLevel.Information,
-                new EventId(2, nameof(InstallationSucceeded)),
+                new EventId(3, nameof(InstallationSucceeded)),
                 "Installation succeeded");
 
         private static readonly Action<ILogger, Exception?> _installationFailed =
             LoggerMessage.Define(
                 LogLevel.Error,
-                new EventId(3, nameof(InstallationFailed)),
+                new EventId(4, nameof(InstallationFailed)),
                 "Installation failed");
 
 
         public static void ServicesToInstall(this ILogger logger, IEnumerable<string> services)
         {
             _servicesToInstall(logger, services, null);
+        }
+
+        public static void UnmatchedServices(this ILogger logger, IEnumerable<string> services)
+        {
+            _unmatchedServices(logger, services, null);
         }
 
         public static void InstallationSucceeded(this ILogger logger)
