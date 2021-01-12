0ea73faf5fc2b37d0e60ce95cba39e726a1ed114
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-11 19:28:52 -0600

    Add source and name to IService, remove start/stop

--- src/Cli/Services/ServiceEntryExtensions.cs
+++ src/Cli/Services/ServiceEntryExtensions.cs
@@ -1,12 +1,17 @@
+using System;
+using System.Collections.Generic;
 using Cli.Services.Configuration;
 
 namespace Cli.Services
 {
     internal static class ServiceEntryExtensions
     {
-        public static IService GetService(this ServiceEntry entry)
+        public static IService GetService(this ServiceEntry entry, IEnumerable<IServiceSource> sources)
         {
-            return new ServiceEntryWrapper(entry);
+            if (string.IsNullOrWhiteSpace(entry.Name))
+                throw new InvalidOperationException("entry.Name must have a value");
+            
+            return new DefaultService(entry.Name, sources);
         }
     }
 }
