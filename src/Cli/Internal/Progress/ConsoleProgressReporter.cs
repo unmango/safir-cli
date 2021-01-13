2777e59326f876283c40a288e0520256ee215fdd
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-12 23:12:23 -0600

    Update console impl to match interface

--- src/Cli/Internal/Progress/ConsoleProgressReporter.cs
+++ src/Cli/Internal/Progress/ConsoleProgressReporter.cs
@@ -3,7 +3,7 @@ using System.CommandLine;
 
 namespace Cli.Internal.Progress
 {
-    public sealed class ConsoleProgressReporter : IProgressReporter
+    internal sealed class ConsoleProgressReporter : IProgressReporter
     {
         private const double Threshold = 69;
         private readonly IConsole _console;
@@ -14,20 +14,7 @@ namespace Cli.Internal.Progress
             _console = console;
         }
 
-        public void Report(string text)
-        {
-            var similar = text.Distance(_prevLine) >= Threshold;
-            // text += text.PadRight(_prevLine.Length);
-            _console.Out.Write($"\r{text}");
-            _prevLine = text;
-        }
-
-        public void Report(int percentage)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Report(float percentage)
+        public void Report(ProgressContext context)
         {
             throw new NotImplementedException();
         }
