0b97d19c2dc1458c1519701f3ecf0c15efffa420
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-12 23:09:51 -0600

    WIP report text

---
+++ src/Cli/Internal/Progress/ProgressReporterExtensions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Cli.Internal.Progress
+{
+    internal static class ProgressReporterExtensions
+    {
+        public static void Report(this IProgressReporter reporter, string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
